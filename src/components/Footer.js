518cc6177ed46cdf919af80df977f0e02864f32c
Will Locklier <will.locklier@gmail.com>
2020-12-21 09:41:35 -0500

    Added updates to clean up template file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer bg-dark">
-            <div className="container text-center">
-                <span className="text-muted">@2020 Built by Rome Bell (during COVID)</span>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer bg-dark">
+      <div className="container text-center">
+        <span className="text-muted">
+          @2020 Created by Zach Finnan, Corey Wilson, Carolina Urrea, and Will
+          Locklier
+        </span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
