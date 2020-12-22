528d74e4033a06b8b70cc9c45eef306c973f1665
Will Locklier <will.locklier@gmail.com>
2020-12-22 11:39:43 -0500

    Add expense amount input field and inital layout of breakdown page

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import About from "./components/About";
 import ExpenseEntry from "./components/ExpenseEntry";
-import Expense from "./components/Expense";
 import Dashboard from "./components/Dashboard";
 import Blogpost from "./components/Blogpost";
+import ExpenseBreakdown from "./components/ExpenseBreakdown";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const user = localStorage.getItem("jwtToken");
@@ -90,9 +90,9 @@ function App() {
           />
           <Route exact path="/" component={Welcome} />
           <Route exact path="/expenseentry" component={ExpenseEntry} />
-          <Route exact path="/expense" component={Expense} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/blogpost" component={Blogpost} />
+          <Route exact path="/expensebreakdown" component={ExpenseBreakdown} />
         </Switch>
       </div>
       <Footer />
---
+++ src/components/ExpenseBreakdown.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const ExpenseBreakdown = () => {
+  const [expenseYear, setExpenseYear] = useState("");
+  const handleExpenseYear = (e) => {
+    setExpenseYear(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // NEED GET ROUTE
+  };
+  return (
+    <div>
+      <div>
+        <h1>Hello from breakdown</h1>
+      </div>
+      <form on onSubmit={handleSubmit}>
+        <label for="expenseMonth">Choose a month</label>
+        <select name="expenseMonth" id="expenseMonth" form="monthForm">
+          <option value="jan">January</option>
+          <option value="feb">February</option>
+          <option value="mar">March</option>
+          <option value="apr">April</option>
+          <option value="may">May</option>
+          <option value="jun">June</option>
+          <option value="jul">July</option>
+          <option value="aug">August</option>
+          <option value="sep">September</option>
+          <option value="oct">October</option>
+          <option value="nov">November</option>
+          <option value="dec">December</option>
+        </select>
+        <label for="expenseYear">Choose a year</label>
+        <input
+          type="number"
+          name="expenseYear"
+          value={expenseYear}
+          onChange={handleExpenseYear}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+      <table>
+        <tr>
+          <th></th>
+          <th>Month to Date</th>
+          <th>Budgeted</th>
+          <th>Surplus/(Overage)</th>
+        </tr>
+        <tr>
+          <th>Rent/Mortgage</th>
+        </tr>
+        <tr>
+          <th>Utilities</th>
+        </tr>
+        <tr>
+          <th>Insurance</th>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default ExpenseBreakdown;
