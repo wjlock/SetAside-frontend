562aad4b1f9ba8562da808a6e9a77f1c3765fb2e
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-12-22 16:07:26 -0500

    [blog] create blog post form and display page

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import About from "./components/About";
 import ExpenseEntry from "./components/ExpenseEntry";
-import Expense from "./components/Expense";
 import Dashboard from "./components/Dashboard";
 import Blogpost from "./components/Blogpost";
+import Blog from "./components/Blog";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const user = localStorage.getItem("jwtToken");
@@ -90,9 +90,9 @@ function App() {
           />
           <Route exact path="/" component={Welcome} />
           <Route exact path="/expenseentry" component={ExpenseEntry} />
-          <Route exact path="/expense" component={Expense} />
           <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/blogpost" component={Blogpost} />
+          <Route exact path="/newblogpost" component={Blogpost} />
+          <Route exact path="/blog" component={Blog} />
         </Switch>
       </div>
       <Footer />
