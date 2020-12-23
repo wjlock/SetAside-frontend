0fe3269d2421083a5fdca446b780583f48aca896
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-12-23 10:21:05 -0500

    [Budget] Is now set up to link and logo and icon are set

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import logo from "../Asset/SetAside.png"
 const Welcome = () => {
   return (
     <div>
       <h1>Welcome to SetAside</h1>
+      <img src={logo} alt="logo-SetAside"/>
     </div>
   );
 };
-
 export default Welcome;
